1d0b415a1ff76727cc93e34fd2de76403b285d7a
Charlotte Schack Berg <csbe@itu.dk>
2022-03-30 10:35:20 +0200

    MultiSet

--- ScrabbleBot/MultiSet.fs
+++ ScrabbleBot/MultiSet.fs
@@ -1,9 +1,39 @@
-﻿// Insert your MultiSet.fs file here. All modules must be internal
+﻿module internal MultiSet
+type MultiSet<'a> when 'a : comparison = M of Map<'a,uint32>
 
-module internal MultiSet
+let empty : MultiSet<'a> = M Map.empty
 
-    type MultiSet<'a> = Temp of unit // Not implemented
+let isEmpty (M m) = Map.isEmpty m
 
-    let empty : MultiSet<'a> = Temp () // Not implemented
-    let add   : 'a -> uint32 -> MultiSet<'a> -> MultiSet<'a> = fun _ _ _ -> failwith "Not implemented" 
-    let fold  : ('b -> 'a -> uint32 -> 'b) -> 'b -> MultiSet<'a> -> 'b = fun _ _ _ -> failwith "Not implemented"
+let size (M m) = Map.fold (fun acc _ v -> acc + v) 0u m
+
+let contains (a:'a) (M m) = Map.containsKey a m
+
+let numItems (a:'a) (M m)=
+    match Map.tryFind a m with
+        | Some a -> a
+        | None -> 0u
+
+let add (a: 'a) x (M m) = Map.add a ((numItems a (M m)) + x) m |> M
+
+let addSingle (a: 'a) (M m) = add a 1u (M m)
+
+let remove (a: 'a) (x: uint32) (M m) =
+    match Map.tryFind a m with
+    | Some v ->
+        if v > x then Map.add a (v - x) m |> M  // return map
+        else Map.remove a m |> M
+    | None -> (M m)
+
+let removeSingle (a: 'a) (M m) = remove a 1u (M m)
+
+// 'b = key
+// 'a = acc
+// uint32 = value
+// f takes the number of occurrences of each element as an argument rather than
+// calling f that number of times
+let fold (f: 'a -> 'b -> uint32 -> 'a) (acc: 'a) ((M m):MultiSet<'b>) =
+    Map.fold f acc m
+    
+let foldBack (f: 'a -> uint32 -> 'b -> 'b) (M m) acc =
+    Map.foldBack f m acc
