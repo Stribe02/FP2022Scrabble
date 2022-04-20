5f42150a6325b5fe22fa64d50c2d5c60d7fef309
DESKTOP-DE23UL5\joann <joannasl95@hotmail.com>
2022-04-20 11:35:41 +0200

    added dict.fs and .fsi

---
+++ ScrabbleBot/Dict.fs
@@ -0,0 +1,16 @@
+﻿module internal Dict
+
+type Dict = Node of Map<char, Dict> * bool
+
+let empty = fun () -> Node(Map.empty, false)
+
+let rec insert word (Node(map, b)) =
+//    match word with
+//    |"" -> Node(map, true)
+//    |s ->
+//        let firstChar = s.[0]
+//        let otherChars = s.[1..]
+//        match Map.tryFind firstChar with
+//        |Some ->
+//            let newNode = insert otherChars dict
+//        |None -> 
