22173a2aafc6c42bb8ebc4c3e5e3bcfb273be302
Charlotte Schack Berg <csbe@itu.dk>
2022-04-29 13:29:17 +0200

    firstChar now in use

--- ScrabbleTemplate/Dict.fs
+++ ScrabbleTemplate/Dict.fs
@@ -2,6 +2,7 @@
 
 type Dict = Node of Map<char, bool * Dict>
 
+let firstChar (s: string) = s.[0]
 let restOfWord (s: string) = s.[1..]
 
 let empty = fun () -> Node(Map.empty)
@@ -9,20 +10,20 @@ let empty = fun () -> Node(Map.empty)
 let insert (word: string) (dict: Dict) =
     let rec aux (w:string) (node: Dict) =
         match node with
-        | Node n when w.Length = 1 && n.ContainsKey w.[0] = false -> Node(Map.add (w.[0]) (true, empty()) n)
-        | Node n when w.Length = 1 -> Node(Map.add (w.[0]) (true, snd(Map.find w.[0] n)) n)
-        | Node n when n.ContainsKey w.[0] -> Node (Map.add (w.[0]) (fst(Map.find w.[0] n), aux (restOfWord w) (snd(Map.find w.[0] n))) n)
-        | Node n when n.ContainsKey w.[0] = false -> Node (Map.add w.[0] (false, aux (restOfWord w) (empty())) n)
+        | Node n when w.Length = 1 && n.ContainsKey (firstChar w) = false -> Node(Map.add (firstChar w) (true, empty()) n)
+        | Node n when w.Length = 1 -> Node(Map.add (firstChar w) (true, snd(Map.find (firstChar w) n)) n)
+        | Node n when n.ContainsKey (firstChar w) -> Node (Map.add (firstChar w) (fst(Map.find (firstChar w) n), aux (restOfWord w) (snd(Map.find (firstChar w) n))) n)
+        | Node n when n.ContainsKey (firstChar w) = false -> Node (Map.add (firstChar w) (false, aux (restOfWord w) (empty())) n)
     aux word dict
     
 let lookup (word: string) (node: Dict) =
     let rec aux (w:string) (nod: Dict) =
         match nod with
-        | Node n when w.Length = 1 -> Map.tryFind w.[0] n |>
+        | Node n when w.Length = 1 -> Map.tryFind (firstChar w) n |>
             function
             | Some n -> true
             | None -> false
-        | Node n when n.ContainsKey w.[0] -> aux (restOfWord w) (snd(Map.find w.[0] n))
+        | Node n when n.ContainsKey (firstChar w) -> aux (restOfWord w) (snd(Map.find (firstChar w) n))
         | Node n -> false
     aux word node
 
