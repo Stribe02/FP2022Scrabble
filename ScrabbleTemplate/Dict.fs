ba11636680d863f2b625d84bf0d17cfaef16cbd8
Charlotte Schack Berg <csbe@itu.dk>
2022-04-29 13:26:10 +0200

    we fixed the trie at last - need to do some makeover

--- ScrabbleTemplate/Dict.fs
+++ ScrabbleTemplate/Dict.fs
@@ -1,38 +1,40 @@
 ﻿module internal Dict
 
-    type Dict =
-        | Node of Map<char, Dict> * bool
+type Dict = Node of Map<char, bool * Dict>
 
-    let restOfWord (s:string) = s.[1..]
+let restOfWord (s: string) = s.[1..]
 
-    let empty = fun() -> Node(Map.empty, false)
+let empty = fun () -> Node(Map.empty)
 
-    let insert (word: string) (dict: Dict) =
-        let rec aux =
+let insert (word: string) (dict: Dict) =
+    let rec aux (w:string) (node: Dict) =
+        match node with
+        | Node n when w.Length = 1 && n.ContainsKey w.[0] = false -> Node(Map.add (w.[0]) (true, empty()) n)
+        | Node n when w.Length = 1 -> Node(Map.add (w.[0]) (true, snd(Map.find w.[0] n)) n)
+        | Node n when n.ContainsKey w.[0] -> Node (Map.add (w.[0]) (fst(Map.find w.[0] n), aux (restOfWord w) (snd(Map.find w.[0] n))) n)
+        | Node n when n.ContainsKey w.[0] = false -> Node (Map.add w.[0] (false, aux (restOfWord w) (empty())) n)
+    aux word dict
+    
+let lookup (word: string) (node: Dict) =
+    let rec aux (w:string) (nod: Dict) =
+        match nod with
+        | Node n when w.Length = 1 -> Map.tryFind w.[0] n |>
             function
-                |[], Node(m,b) -> Node (m, true)
-                |firstChar::restOfWord, Node (m, b) ->
-                    match m.TryFind firstChar with
-                    | Some (Node (n', b')) ->
-                        let node = Node(m, b')
-                        let newMap = m.Add (firstChar, aux (restOfWord, empty()))
-                        Node (newMap, b)
-                    | None ->
-                        let newMap = m.Add (firstChar, aux (restOfWord, empty()))
-                        Node (newMap, b)
-        aux (Seq.toList word, dict)
-        
-    let step (ch: char) (dict: Dict) =
-        match dict with
-        | Node(m,b) when m.ContainsKey ch ->
-            let x = Map.find ch m
-            match x with
-            | Node(m', b') -> Some (b', x)
-        | _ -> None
+            | Some n -> true
+            | None -> false
+        | Node n when n.ContainsKey w.[0] -> aux (restOfWord w) (snd(Map.find w.[0] n))
+        | Node n -> false
+    aux word node
 
-    let rec lookup word =
-        function
-            |Node(_, b) when word = "" -> b
-            |Node(map, _) -> lookup (restOfWord word) (Map.find word.[0] map)
-        
-                
+let step (c : char) (node : Dict) = 
+        let rec inner (c : char) (nod : Dict) = 
+            match nod with
+            | Node x when x.ContainsKey c  = false -> None  
+            | Node x -> Map.tryFind c x |>
+                function
+                | Some (b, dict) -> 
+                    match dict with
+                    | Node d when b -> Some(b, dict)
+                    | _ -> Some(b, dict)
+                | None -> None
+        inner c node
